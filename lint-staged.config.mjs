10b13dbd08bf50e13ee880e598047ed4eaf9ea1c
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-07-26 21:19:36 +0900

    chore: setup build environment

---
+++ lint-staged.config.mjs
@@ -0,0 +1,6 @@
+export default {
+  '**/*.(mts|ts|tsx|js)': (filenames) => [
+    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
+  ],
+};
---
+++ tests/index.unit.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe("index.unit.test.mts", () => {
+  it("should run", () => {
+    expect(true).toBe(true);
+  });
+});
