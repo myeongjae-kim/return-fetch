4853ad8e9cc043a4f22a57c76627f45641673f8a
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-08-05 22:16:31 +0900

    chore: add set-version script and set verison to 0.4.3

---
+++ scripts/set-version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+[[ $1 ]] || { echo "Usage: npm set-version <version>"; exit 1; }
+
+VERSION=$1
+[[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] || { echo "Version is invalid. Version must be numbers separated by two dots."; exit 1; }
+
+# set package.json version
+cat package.json | jq ".version = \"$VERSION\"" > package.json.tmp
+rm package.json && mv package.json.tmp package.json
+
+files=$(find packages -type f -name "package.json");
+
+for file in $files; do
+  cat "$file" | jq ".version = \"$VERSION\"" > "$file.tmp"
+  rm "$file" && mv "$file.tmp" "$file"
+done
